979554e633882395c564db355684ccc04d9847e8
Victor <victorzevallos@protonmail.com>
2020-10-23 23:32:34 -0500

    Update views

--- src/main/java/com/victorze/secondhandmarket/security/UserDetailServiceImplementation.java
+++ src/main/java/com/victorze/secondhandmarket/security/UserDetailServiceImplementation.java
@@ -1,14 +1,17 @@
 package com.victorze.secondhandmarket.security;
 
-import com.victorze.secondhandmarket.models.User;
-import com.victorze.secondhandmarket.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User.UserBuilder;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+import com.victorze.secondhandmarket.models.User;
+import com.victorze.secondhandmarket.repositories.UserRepository;
+
+@Service()
 public class UserDetailServiceImplementation implements UserDetailsService {
 
     @Autowired
@@ -18,14 +21,19 @@ public class UserDetailServiceImplementation implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findFirstByEmail(username);
 
+        UserBuilder builder = null;
+
         if (user != null) {
-            UserBuilder builder = org.springframework.security.core.userdetails.User.withUsername(username);
+            builder = org.springframework.security.core.userdetails.User.withUsername(username);
             builder.disabled(false);
             builder.password(user.getPassword());
             builder.authorities(new SimpleGrantedAuthority("ROLE_USER"));
-            return builder.build();
+//            return builder.build();
+        } else {
+        	throw new UsernameNotFoundException("User '" + username + "' not found");
         }
-        throw new UsernameNotFoundException("User '" + username + "' not found");
+        
+        return builder.build();
     }
 
 }
