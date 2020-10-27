5b00f5c0dd961f2c622dd3c49c1809f28d885ad1
Victor <victorzevallos@protonmail.com>
2020-10-26 23:44:05 -0500

    Add file storage service

---
+++ src/main/java/com/victorze/secondhandmarket/upload/StorageFileNotFoundException.java
@@ -0,0 +1,13 @@
+package com.victorze.secondhandmarket.upload;
+
+public class StorageFileNotFoundException extends StorageException {
+
+	public StorageFileNotFoundException(String message) {
+		super(message);
+	}
+
+	public StorageFileNotFoundException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+}
---
+++ src/main/java/com/victorze/secondhandmarket/upload/StorageProperties.java
@@ -0,0 +1,18 @@
+package com.victorze.secondhandmarket.upload;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix="storage")
+public class StorageProperties {
+
+	private String location = "upload-dir";
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+}
