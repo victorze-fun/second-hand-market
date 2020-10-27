408d2588d9b91ab31fc8895f5f679a55ce4cb6d8
Victor <victorzevallos@protonmail.com>
2020-10-26 23:45:27 -0500

    Add new product form and FilesController

--- src/main/java/com/victorze/secondhandmarket/config/ConfigurationAudit.java
+++ src/main/java/com/victorze/secondhandmarket/config/ConfigurationAudit.java
@@ -1,9 +1,13 @@
 package com.victorze.secondhandmarket.config;
 
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import com.victorze.secondhandmarket.upload.StorageProperties;
+
 @Configuration
 @EnableJpaAuditing
+@EnableConfigurationProperties(StorageProperties.class)
 public class ConfigurationAudit {
 }
